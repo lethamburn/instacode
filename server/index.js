43dc5455e389c60eb20ad13210f51f2429ff44db
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-11-15 16:00:55 +0100

    FEAT SERVER & DEPENDENCIES

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const { connect } = require("./config/db");
+
+connect();
+
+const app = express();
+
+const PORT = 3000;
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:4200"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
